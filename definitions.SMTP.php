be435fb66a8bbe643dfceca32ebce308cc55ec54
Ammar Alakkad <am.alakkad@gmail.com>
2014-08-28 16:54:56 +0300

    Add SMTP translation

---
+++ definitions.SMTP.php
@@ -0,0 +1,12 @@
+<?php
+// Copyright 2013 Toby Zerner, Simon Zerner
+// This file is part of esoTalk. Please see the included license file for usage information.
+
+// English Definitions for the SMTP plugin.
+
+$definitions["Server"] = "مخدم";
+$definitions["Port"] = "منفذ";
+$definitions["Authentication"] = "المصادقة";
+$definitions["Normal"] = "عادي";
+$definitions["TLS"] = "TLS";
+$definitions["SSL"] = "SSL";
