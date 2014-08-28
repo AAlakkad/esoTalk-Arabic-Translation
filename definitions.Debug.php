bf9727e33dd8f4ac83c760f9e01ca8c98be6dfff
Ammar Alakkad <am.alakkad@gmail.com>
2014-08-28 16:54:48 +0300

    Add Debug translation

---
+++ definitions.Debug.php
@@ -0,0 +1,10 @@
+<?php
+// Copyright 2011 Toby Zerner, Simon Zerner
+// This file is part of esoTalk. Please see the included license file for usage information.
+
+// English Definitions for the Debug plugin.
+
+$definitions["MySQL queries"] = "استعلامات MySQL";
+$definitions["Page loaded in %s seconds"] = "تم تحميل الصفحة خلال %s ثانية";
+$definitions["POST + GET + FILES information"] = "POST + GET + FILES معلومات";
+$definitions["SESSION + COOKIE information"] = "SESSION + COOKIE معلومات";
