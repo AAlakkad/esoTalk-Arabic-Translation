4b539826889190e23705e5e3c08a4c5955e8ea63
Ammar Alakkad <am.alakkad@gmail.com>
2014-08-28 16:54:37 +0300

    Add BBCode translation

---
+++ definitions.BBCode.php
@@ -0,0 +1,13 @@
+<?php
+// Copyright 2011 Toby Zerner, Simon Zerner
+// This file is part of esoTalk. Please see the included license file for usage information.
+
+// English Definitions for the BBCode plugin.
+
+$definitions["Code"] = "كود";
+$definitions["Image"] = "صورة";
+$definitions["Link"] = "رابط";
+$definitions["Strike"] = "خط منصف";
+$definitions["Header"] = "عنوان";
+$definitions["Italic"] = "مائل";
+$definitions["Bold"] = "عريض";
